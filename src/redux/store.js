3e6da855ce3a296bf216d0824bd1fda224d84be3
Niranjana9588 <niranjanap334@gmail.com>
2023-12-20 00:58:17 +0530

    final

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+const { configureStore } = require("@reduxjs/toolkit");
+import restaurantSlice from '../redux/restaurantSlice'
+
+const store = configureStore({
+    reducer:{
+       restaurantSlice
+    }
+})
+export default store
